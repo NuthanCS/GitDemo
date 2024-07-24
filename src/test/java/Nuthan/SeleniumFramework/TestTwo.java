a0b7eef355b360a9e68ab0293850201a1d5b21a4
nuthan <csnuthan@gmail.com>
2024-07-25 00:50:29 +0530

    develop: removed sub method from testTwo

--- src/test/java/Nuthan/SeleniumFramework/TestTwo.java
+++ src/test/java/Nuthan/SeleniumFramework/TestTwo.java
@@ -5,17 +5,10 @@ public class TestTwo {
 	public static void main(String[] args) {
 		TestTwo testTwo = new TestTwo();
 		testTwo.add(1);
-		testTwo.sub('a');
 	}
 	
 	public void add(int a)
 	{
 		System.out.println("integer");
 	}
-	
-	public void sub(char a)
-	{
-		System.out.println("character");
-	}
-
 }
